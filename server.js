40f9db66e7f67c5db89f011b0a90b4deab6ae1c1
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-08-27 10:47:59 +0200

    chatFixed
    chat are fixed with socket.io och express

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server, {pingTimeout: 25000})
+
+// Static folder
+app.use(express.static(__dirname + '/client'));
+
+// Run when client connects
+io.on('connection', socket => {
+    // ### Test New user connection ### //
+    //console.log('New Connection....');
+
+    // Welcome Message
+    socket.emit('message', 'Välkommen')
+
+    // Broadcst when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when client disconnects
+    socket.on('disconnects', () =>{
+        io.emit('message', 'A user has left the chat')
+    });
+
+    // Listen chat message
+    socket.on('chatMessage', (msg) =>{
+        io.emit('message', msg)
+    })
+
+});
+
+const PORT = 3000 || process.env.PORT
+
+server.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`))
